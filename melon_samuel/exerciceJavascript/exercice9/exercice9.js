235c3938e13e713e2d1f94a834341cac1487cfdf
MouradAfpa <mourad.arab.formateur.afpa@gmail.com>
2024-02-06 14:45:21 +0100

    ajout correction de Samuel dans formateur

--- melon_samuel/exerciceJavascript/exercice9/exercice9.js
+++ melon_samuel/exerciceJavascript/exercice9/exercice9.js
@@ -1,5 +1,7 @@
-function transforme(texte) {
+function accum(texte) {
     return texte.split('').map((lettre, index) => lettre.toUpperCase() + lettre.toLowerCase().repeat(index)).join('-');
 }
 
-console.log(transforme('abcd'));
+console.log(accum('abcd'));
+console.log(accum("RqaEzty"));
+console.log(accum("cwAt") );

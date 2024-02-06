70d40407ff9b122cae1085a5f8a7765ccf2f2264
LasueQuentin <quentin.lasue.jm@gmail.com>
2024-02-06 14:51:26 +0100

    Modif accumulation.js exercice 9

--- lasue_quentin/ExerciceJavaScript/Exercice9/accumulation.js
+++ lasue_quentin/ExerciceJavaScript/Exercice9/accumulation.js
@@ -1,9 +1,9 @@
 function accum(chaine){
 
-    //  return chaine.split("").map((c,i) => {return (c.toUpperCase()+c.toLowerCase().repeat(i));}).join("-");
-    return chaine.replace(/./g,function(c,i){
-        return c.toUpperCase()+c.toLowerCase().repeat(i) +"-";
-    });
+     return chaine.split("").map((c,i) => {return (c.toUpperCase()+c.toLowerCase().repeat(i));}).join("-");
+    // return chaine.replace(/./g,function(c,i){
+    //     return c.toUpperCase()+c.toLowerCase().repeat(i) +"-";
+    // });
 }
 
 console.log(accum("abcd"));

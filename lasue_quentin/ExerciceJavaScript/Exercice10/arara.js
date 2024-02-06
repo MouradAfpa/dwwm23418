8ca551df0778539a1f4bd19c6e35fc1960dc58d3
LasueQuentin <quentin.lasue.jm@gmail.com>
2024-02-06 16:32:48 +0100

    modification exercice 10

--- lasue_quentin/ExerciceJavaScript/Exercice10/arara.js
+++ lasue_quentin/ExerciceJavaScript/Exercice10/arara.js
@@ -2,8 +2,8 @@ function arara(num){
 
     let pair = num%2 ===0;
     let adak = pair?num/2 :(num-1)/2;
-    let res = "adak ".repeat(adak)+(num%2!==0 ?"anane":"");
-    
+    let res = ("adak ".repeat(adak)+(num%2!==0 ?"anane":"")).trim();
+    //trim nettoie le debut et la fin si des espaces vides
     return res
 }
 console.log(arara(13));

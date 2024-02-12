d3199d2e41f09abb95c71e710a86272b1ae3b030
joJosh59 <joshua97@hotmail.fr>
2024-02-12 18:02:27 +0100

    ajout module et legere modif sur cardFilm.js

--- loeuil_joshua/projet-react-test/src/Moviesearch/Composant/cardFilm.js
+++ loeuil_joshua/projet-react-test/src/Moviesearch/Composant/cardFilm.js
@@ -1,7 +1,5 @@
 import React from "react";
 import  Card  from "react-bootstrap/Card";
-import  CardGroup  from "react-bootstrap/CardGroup";
-import axios from "axios";
 
 function CardFilm(){
     return (
@@ -11,10 +9,9 @@ function CardFilm(){
                 <Card border="success">
                     <Card.Img variant="top" src=""/>
                     <Card.Body>
-                        <Card.Title>Primary Card Title</Card.Title>
+                        <Card.Title>Titre film</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        leger synopsis
                         </Card.Text>
                         </Card.Body>
                 </Card>

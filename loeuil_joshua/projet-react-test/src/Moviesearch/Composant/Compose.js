9aefc5642a215922fd1843b0faff636942933355
joJosh59 <joshua97@hotmail.fr>
2024-02-12 18:13:21 +0100

    ajout sur les composant CardFilm

--- loeuil_joshua/projet-react-test/src/Moviesearch/Composant/Compose.js
+++ loeuil_joshua/projet-react-test/src/Moviesearch/Composant/Compose.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./search";
+import CardFilm from "./cardFilm";
 
 
 function ComposerComposent(){
     return (
+        <>
         <Navbar/>
+        <CardFilm/>
+        </>
     )
 }
 

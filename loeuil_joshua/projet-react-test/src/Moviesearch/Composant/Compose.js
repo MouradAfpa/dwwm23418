772067b718d5f281669a63dbc59f4a407ac5e25a
joJosh59 <joshua97@hotmail.fr>
2024-02-12 16:18:50 +0100

    ajout composant pour regrouper tous composant creer plus tard

---
+++ loeuil_joshua/projet-react-test/src/Moviesearch/Composant/Compose.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./search";
+
+
+function ComposerComposent(){
+    return (
+        <Navbar/>
+    )
+}
+
+export default ComposerComposent;
--- loeuil_joshua/projet-react-test/src/index.js
+++ loeuil_joshua/projet-react-test/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './Moviesearch/Composant/search';
+import ComposerComposent from './Moviesearch/Composant/Compose';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
+    <ComposerComposent />
   </React.StrictMode>
 );
 

a7eeb7057dbfce49455988613cf0f42a971695d3
LasueQuentin <quentin.lasue.jm@gmail.com>
2024-02-19 15:48:50 +0100

    Simplification du code et des component

--- lasue_quentin/projet_react/movie-app/src/component/AffichageFilm.js
+++ lasue_quentin/projet_react/movie-app/src/component/AffichageFilm.js
@@ -1,14 +1,10 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-import { Col,NavbarBrand,Row } from 'react-bootstrap';
 import FilmCards from './CardsFilm';
-import ListeFilm from './ListeFilm';
-
 
 
 
@@ -19,30 +15,40 @@ function AffichageFilms() {
     const [nbrPage,setNbrPage] = React.useState('1');
 
     useEffect(()=>{
-        const getFilm = async ()=>{
-            try{
-            const movie = await axios.get(
-                `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${searchVal}&page=${pageActuelle}`
-            );
-    
-            setMovies(movie.data.results);
-            setNbrPage(movie.data.total_pages)
-            }catch(error){
-            console.log('Error fetching data: ',error);
+        if(!searchVal){
+            const getFilms = async () => {
+                try{
+                    const movies = await axios.get(
+                        `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_API_KEY}&page=${pageActuelle}`);
+                    
+                    setMovies(movies.data.results);
+                    setNbrPage(movies.data.total_pages)
+                }catch (error){
+                    console.log('error fetching movies:', error);
+                }
+            };
+            getFilms();
+        }else{
+            const getFilm = async ()=>{
+                try{
+                const movie = await axios.get(
+                    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${searchVal}&page=${pageActuelle}`
+                );
+        
+                setMovies(movie.data.results);
+                setNbrPage(movie.data.total_pages)
+                }catch(error){
+                console.log('Error fetching data: ',error);
+                }
             }
+            getFilm();
         }
-        getFilm();
     },[pageActuelle,searchVal]); // à l'écoute des changements de Page et search Val
-
-    const searchMovie = (searchVal) => {
-            setSearchVal(searchVal);
-            setPage(1); // Reset pageActuelle à 1 los de la nouvele recherche
-        };
 
     const changeRecherche = (e) => {    //a l'écoute de l'événement dès qu'il y a un changement, on envoi la nouvelle valeur dans searchVal 
         const searchVal = e.target.value;
         setSearchVal(searchVal);
-        searchMovie(searchVal);
+        setPage(1); //Reset la page actuelle
         };
 
     const nextPage = () => {
@@ -59,7 +65,7 @@ function AffichageFilms() {
     };    
 
     return(
-        <>
+    <>
         <Navbar className="bg-dark justify-content-center" data-bs-theme="dark" fixed="top">
         <Container>
           <Navbar.Brand> <span className="text-warning fw-bold">Search</span> <i className="text-info">Films</i></Navbar.Brand>
@@ -75,13 +81,18 @@ function AffichageFilms() {
         
     
         <div className="container mt-5">
-            
-            {(movies.length ===0)||(searchVal==='')  ?
-            (<div className="row justify-content-center">
-            <ListeFilm/>
+            <div className='row'>
+                    <div className='col'>
+                        <Button variant="outline-info" onClick={previousPage} size='lg'>Page précédente</Button>
+                    </div>
+                    <div className='col'>
+                        <Button variant="outline-info" size='lg'>Page {pageActuelle} sur {nbrPage}</Button>
+                    </div>
+                    <div className='col'>
+                        <Button variant="outline-info" onClick={nextPage} size='lg'>Page suivante</Button>
+                    </div>
             </div>
-            ):
-            (<>
+            
             <div className="row justify-content-center">
             {movies.map((film)=>(
                 
@@ -89,19 +100,17 @@ function AffichageFilms() {
             </div>
             <div className='row'>
                 <div className='col'>
-                <Button variant="outline-info" onClick={previousPage} size='lg'>Page précédente</Button>
+                    <Button variant="outline-info" onClick={previousPage} size='lg'>Page précédente</Button>
                 </div>
                 <div className='col'>
-                <Button variant="outline-info" size='lg'>Page {pageActuelle}</Button>
-                <Button variant="outline-info" size='lg'>Total pages :{nbrPage}</Button>
+                    <Button variant="outline-info" size='lg'>Page {pageActuelle} sur {nbrPage}</Button>
                 </div>
                 <div className='col'>
-                <Button variant="outline-info" onClick={nextPage} size='lg'>Page suivante</Button>
+                    <Button variant="outline-info" onClick={nextPage} size='lg'>Page suivante</Button>
                 </div>
-             </div></>
-                )}
+             </div>
         </div>
-        </>
+    </>
     )
 }
 
--- lasue_quentin/projet_react/movie-app/src/component/CardsFilm.js
+++ lasue_quentin/projet_react/movie-app/src/component/CardsFilm.js
@@ -11,10 +11,16 @@ function FilmCards(props) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const image = props.movie.poster_path ? `https://image.tmdb.org/t/p/w500${props.movie.poster_path}` 
+  : `https://thumbs.dreamstime.com/b/popcorn-soda-takeaway-d-cinema-glasses-design-illus-illustration-eps-75522958.jpg`;
+  
   return (
     <>
     <Card border="info" style={{ width: '18rem' }} className='m-5 border-2 bg-black' onClick={handleShow}>
-      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`} alt={props.movie.title} />
+      <Card.Img 
+      variant="top" 
+      src={image} 
+      alt={props.movie.title} />
       <Card.Body>
         <Card.Title className='text-light'>{props.movie.title}</Card.Title>
         <Button variant="outline-info">En savoir plus</Button>

23c69eb3e9f0b588af71e9ff05b4eac9d7da27ca
Keklevin <nerokaze62@gmail.com>
2024-02-21 15:23:54 +0100

    ajout exercice 15 challenges js

--- cleret_kevin/ExerciceJavaScript/Exercice15/script.js
+++ cleret_kevin/ExerciceJavaScript/Exercice15/script.js
@@ -3,16 +3,16 @@ function counterEffect(txt) {
     let regex = txt.split('');
     let tab = [];
     let tabReponse = new Array();
-    let i=0;
-    let j=0;
 
-    for (i=0; i<regex.length ; i++) {
-        tab.push(parseInt(regex[i]));
+    for (let i=0; i<regex.length ; i++) {
+        // tab.push(parseInt(regex[i]));        //ne pas faire attention à cette ligne !
         tabReponse[i] = new Array();
     }
-
-    for (j=0; j<tab.length ; j++) {
-        tab[i][]
+    
+    for (let k=0; k<tabReponse.length; k++) {
+        for (let j=0; j<=regex[k] ; j++) {
+            tabReponse[k][j] = j;
+        }
     }
 
     return tabReponse;
@@ -20,6 +20,7 @@ function counterEffect(txt) {
 
 
 console.log(counterEffect('12458'))
+
 
 
 

bf1437c8b22a2a5da8dfdc41e95033628531e5e5
joJosh59 <joshua97@hotmail.fr>
2024-02-22 14:20:53 +0100

    action et index

--- loeuil_joshua/projet-react-test/src/test/Redux/actions.js
+++ loeuil_joshua/projet-react-test/src/test/Redux/actions.js
@@ -1,20 +1,66 @@
-const fetchFilmPop = 'fetchFilmPop';
-const rechercheFilm = 'rechercheFilm';
+import axios from "axios";
+
+export const fetchFilmPop = "fetchFilmPop";
+export const rechercheFilm = "rechercheFilm";
+export const prev_Page = "prev_Page";
+export const next_Page = "next_Page";
+export const reset_Page = "reset_Page";
 
 const api_key = process.env.REACT_APP_API_KEY;
 
 export const fetchFilmPops = () => {
-  return (dispatch) => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`)
-      .then(response => response.json())
-      .then(data => dispatch({ type: fetchFilmPop, payload: data.results }));
+  return async (dispatch, getState) => {
+    const { page } = getState().Reducer1;
+    try {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=${api_key}&page=${page}`
+      );
+      const { data } = res;
+      dispatch({
+        type: fetchFilmPop,
+        payload: {
+          results: data.results,
+          page: data.page,
+          total_pages: data.total_pages,
+        },
+      });
+    } catch (error) {
+      console.error("Error query films:", error);
+    }
   };
 };
 
-export const rechercheFilms = (recherche) => {
-  return (dispatch) => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${recherche}`)
-      .then(response => response.json())
-      .then(data => dispatch({ type: rechercheFilm, payload: { recherche, films: data.results } }));
+export const rechercheFilms = (query, page) => {
+  return async (dispatch, getState) => {
+    // const { page } = getState().Reducer1;
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}&page=${page}`
+      );
+      const { data } = response;
+      dispatch({
+        type: rechercheFilm,
+        payload: {
+          query,
+          results: data.results,
+          page: data.page,
+          total_pages: data.total_pages,
+        },
+      });
+    } catch (error) {
+      console.error("Error query films:", error);
+    }
   };
 };
+
+export const prevPage = () => ({
+  type: prev_Page,
+});
+
+export const nextPage = () => ({
+  type: next_Page,
+});
+
+export const resetPage = () => ({
+  type: reset_Page,
+});

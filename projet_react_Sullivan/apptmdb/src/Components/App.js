e73c19a4ec5286c8ab1825d2c80dceef4d172ad1
Sulli <sullivan.richard@hotmail.fr>
2024-02-23 16:10:26 +0100

    projet react 23/02/2024

---
+++ projet_react_Sullivan/apptmdb/src/Components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchBar from './SearchBar';
+import Banner from './Banner';
+
+function App() {
+  return (
+    <div className="App">
+        <Banner />
+        <SearchBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ projet_react_Sullivan/test/my-test/src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import Form from 'react-bootstrap/Form';
+
+
+
+const SearchBox = () => {
+    return <Form.Control size="lg" type="text" placeholder="Recherche" />
+}
+
+export default SearchBox;

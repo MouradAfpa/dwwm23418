4a10c71634e6f239adc5b43055b1246767b17e81
joJosh59 <joshua97@hotmail.fr>
2024-02-26 14:41:15 +0100

    modif sur Exercice Fizz_Buzz et ajout Exercice juste Prix

--- loeuil_joshua/production_eval_js/Eval2/Fizz_Buzz/fizzbuzz.js
+++ loeuil_joshua/production_eval_js/Eval2/Fizz_Buzz/fizzbuzz.js
@@ -7,13 +7,13 @@ function fizzbuzz(){
     let bu= "BUZZ";
     let fibu ="FIZZBUZZ";
 
-   for(let i = 0; i < N; i++){
-    if(i % 3 == 0){
-        mult = fi
+   for(let i = 0; i <= N; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        mult = fibu
     }else if(i % 5 == 0){
         mult = bu
-    }else if(i % 3 == 0 && i % 5 == 0){
-        mult=fibu
+    }else if(i % 3 == 0){
+        mult=fi
     }else{
         mult = i;
     };

44b6b1d6cbf4ec5a3b795e3dcc65360e4b37f9ce
Th3B1gL3b0wsk1 <laporte.matthieu08@gmail.com>
2024-02-26 11:42:55 +0100

    Ajout du répertoire production_eval_js

---
+++ laporte_matthieu/production_eval_js/js/laporte_matthieu_eval_js.js
@@ -0,0 +1,121 @@
+// --> EXO1
+let nom = prompt("Donnez votre nom");
+
+document.write("Bienvenue " + nom);
+
+
+
+
+
+// --> EXO2
+
+// let nombreA = parseInt(prompt("Entrez un nombre"));
+// let nombreB = parseInt(prompt("Entrez un second nombre"));
+
+// let result = nombreA + nombreB;
+// alert(nombreA + " + " + nombreB + " = " + result);
+
+
+
+
+
+// --> EXO3
+
+// let fabrication = parseInt(prompt("Entrez le prix de fabrication"));
+// let vente = parseInt(prompt("Entrez le prix de vente"));
+
+// if(fabrication > vente) {
+//     alert("Perte");
+// }
+// else {
+//     alert("Profit");
+// }
+
+
+
+// --> EXO4
+
+// let premier = parseInt(prompt("Entrez le premier nombre"));
+// let second = parseInt(prompt("Entrez le second nombre"));
+// let troisième = parseInt(prompt("Entrez le troisième nombre"));
+
+// if((premier > second) && (premier > troisième)) {
+//     alert("Le plus grand nombre est " + premier);
+// }
+// else if((second > premier) && (second > troisième)) {
+//     alert("Le plus grand nombre est " + second);
+// }
+// else {
+//     alert("Le plus grand nombre est " + troisième);
+// }
+
+
+
+// --> EXO5
+
+// let noteEleve = parseInt(prompt("Entrez la note"));
+
+// if(noteEleve <= 4) {
+//     alert("Catastrophique, il faut tout revoir");
+// }
+// else if((noteEleve >= 5) && (noteEleve <= 10)) {
+//     alert("Insuffisant");
+// }
+// else if((noteEleve >= 11) && (noteEleve <= 14)) {
+//     alert("Peut mieux faire");
+// }
+// else if((noteEleve >= 15) && (noteEleve <= 17)) {
+//     alert("Bien");
+// }
+// else {
+//     alert("Excellent, bon travail");
+// }
+
+
+
+// --> EXO6
+
+// let calcul1 = parseInt(prompt("Entrez le premier nombre"));
+// let operateur = prompt("Entrez un opérateur (+, *, / ou -)");
+// let calcul2 = parseInt(prompt("Entrez le second nombre"));
+// let result;
+
+// switch (operateur) {
+//     case "+":
+//         result = calcul1 + calcul2;
+//         alert(calcul1 + " + " + calcul2 + " = " + result);
+//         break;
+    
+//     case "*":
+//         result = calcul1 * calcul2;
+//         alert(calcul1 + " x " + calcul2 + " = " + result);
+//         break;
+
+//     case "/":
+//         result = calcul1 / calcul2;
+//         alert(calcul1 + " / " + calcul2 + " = " + result);
+//         break;
+
+//     case "-":
+//         result = calcul1 - calcul2;
+//         alert(calcul1 + " - " + calcul2 + " = " + result);
+//         break;
+
+//     default:
+//         alert("Erreur, mauvais opérateur, réessayez");
+
+// }
+
+
+// --> EXO7
+
+// let n = parseInt(prompt("Entrez un nombre"));
+// let i = 0;
+// let result = 0;
+
+// while(i < n) {
+//     i++;
+//     result = result + i;  
+// }
+// alert(result);
+

e97fadfd8fc4046cf800dc8f5b044c2c55a9289d
Stan <154512651+StanislasCasamajor@users.noreply.github.com>
2024-02-26 11:45:06 +0100

    ajout exos

--- casamajor_stanislas/production_eval_js/Exercice 7/somme07.js
+++ casamajor_stanislas/production_eval_js/Exercice 7/somme07.js
@@ -1,12 +1,14 @@
 function somme(){
 let n = parseInt(prompt("Entrer un entier"));
 let cumul = 0;
+let str = "";
 let i;
 
     for(i=1; i <=n; i++){
         cumul += i 
-    }
+        str = str + '+' + i;
+    } 
 
-    document.write("La somme jusqu'a " + n + " est : " + cumul)
+    document.write(str + "=" + cumul)
 }
 somme();

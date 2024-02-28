1398be794699f0496c40ef3dbd09d221c5615805
Sulli <sullivan.richard@hotmail.fr>
2024-02-28 10:00:59 +0100

    delete_d*oublon

--- projet_react_Sullivan/apptmdb/src/Components/Banner.jsx
+++
@@ -1,3 +0,0 @@
-export default function Banner() {
-    return <h1>Cinéma</h1>
-}
--- projet_react_Sullivan/test/my-test/src/components/SearchBox.js
+++
@@ -1,9 +0,0 @@
-import Form from 'react-bootstrap/Form';
-
-
-
-const SearchBox = () => {
-    return <Form.Control size="lg" type="text" placeholder="Recherche" />
-}
-
-export default SearchBox;

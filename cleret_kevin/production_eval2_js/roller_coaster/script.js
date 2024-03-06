5b4ec56096bec4b89e0841715f25ff869ed9d8b0
Keklevin <nerokaze62@gmail.com>
2024-03-06 09:45:29 +0100

    modif sur rollercoaster

--- cleret_kevin/production_eval2_js/roller_coaster/script.js
+++ cleret_kevin/production_eval2_js/roller_coaster/script.js
@@ -1,28 +1,30 @@
 
 function rollerCosta() {
-    let places = 5;
     let tours = 3;
     let benef = 0;
-    let file = [2,3,5,4];
+
+    // let file = [1,2,3,5,4,1,2];
+    let file = [5,2,3,4];
     let placesR = 0;
+    console.log(file);
 
     for (i=0; i<tours; i++) {
         placesR = file[0];
         permute(file);
-
-        while(placesR <= places) {
-            if ((placesR + file[0]) <= places) {
+        
+        while(placesR <= 5) {
+            if ((placesR + file[0]) <= 5) {
                 placesR += file[0];
-                permute(file);   
+                permute(file);  
             } else {
-                placesR;
+                // placesR;
                 break;
             }
         }
 
-        benef = benef + placesR;
-        console.log(file);
+        benef += placesR;
         console.log(`benefices : ${benef}`);
+        console.log(file);
     }
 }
 

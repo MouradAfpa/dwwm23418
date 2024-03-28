2078666f6c2da550c806b0a28cfc9b040f6724de
LasueQuentin <quentin.lasue.jm@gmail.com>
2024-03-28 13:04:56 +0100

    Commentage du code

--- lasue_quentin/ExercicePHP/gestionTroupeMusic/index.php
+++ lasue_quentin/ExercicePHP/gestionTroupeMusic/index.php
@@ -184,13 +184,10 @@
             ?>
 
         </div>
-        <!-- Début du travail sur la photo de profil
-        Reste a : créer dans database un methode qui envoi la photo dans une colonne photo
-                dans le traitement vérifier avant d'appeler la method  -->
 
     </div>
     <!-- <script>
-        src = "./script.js"
+        src = "script.js"
     </script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
--- lasue_quentin/ExercicePHP/gestionTroupeMusic/traitementForm.php
+++ lasue_quentin/ExercicePHP/gestionTroupeMusic/traitementForm.php
@@ -1,40 +1,65 @@
 <?php
+// Inclure le fichier contenant la classe Database
 include "Database.php";
 
+// Vérifier si les données pour l'ajout sont présentes dans $_POST
 if (
     isset($_POST['nom']) &&
     isset($_POST['age']) &&
     isset($_POST['type'])
 ) {
+    // Afficher un message de confirmation
     echo "Formulaire Ajout valide.";
+
+    // Récupérer et nettoyer les données du formulaire
     $nom = htmlspecialchars($_POST['nom']);
     $age = htmlspecialchars($_POST['age']);
     $type = $_POST['type'];
+
+    // Créer une instance de la classe Database
     $db = new Database();
+
+    // Appeler la méthode d'ajout de musicien de la classe Database
     $db->ajoutMusicien($type, $nom, $age);
 }
 
+// Vérifier si les données pour la modification sont présentes dans $_POST
 if (
     isset($_POST['id']) &&
     isset($_POST['attribut']) &&
     isset($_POST['newNom']) &&
     isset($_POST['newAge'])
 ) {
+    // Afficher un message de confirmation
     echo "Formulaire Modif valide.";
+
+    // Récupérer et nettoyer les données du formulaire
     $id = $_POST['id'];
     $attribut = $_POST['attribut'];
     $newNom = htmlspecialchars($_POST['newNom']);
     $newAge = htmlspecialchars($_POST['newAge']);
 
+    // Créer une instance de la classe Database
     $db = new Database();
+
+    // Appeler la méthode de mise à jour des membres de la classe Database
     $db->updateMember($id, $attribut, $newNom, $newAge);
 }
 
+// Vérifier si l'ID pour la suppression est présent dans $_POST
 if (isset($_POST['idSup'])) {
+    // Récupérer l'ID de la suppression
     $idSup = $_POST['idSup'];
+
+    // Afficher un message de confirmation
     echo "Suppression effectuer.";
+
+    // Créer une instance de la classe Database
     $db = new Database();
+
+    // Appeler la méthode de suppression de membre de la classe Database
     $db->deleteMember($idSup);
 }
 
+// Rediriger vers la page index.php après le traitement des formulaires
 header("Location:index.php");

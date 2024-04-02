b1af6f93e7cdddaa499e5fd1ff4ae4b369c0e7d8
Delhumeur <126495668+Delhumeur@users.noreply.github.com>
2024-04-02 16:33:56 +0200

    ajout MVC

--- dervaux_julien/METIS/MVC/controllers/Articles.php
+++ dervaux_julien/METIS/MVC/controllers/Articles.php
@@ -1,5 +1,5 @@
 <?php
 
-class Articles{
-    
+class Articles extends Controller{
+
 }
---
+++ dervaux_julien/METIS/MVC/controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+
+abstract class Controller{
+
+    public function homeView(){
+        include("views/home.php");
+    }
+    public function homeLogin(){
+        include("views/login.php");
+    }
+    public function homeView(){
+        include("views/");
+    }
+    public function homeView(){
+        include("views/");
+    }
+
+}
